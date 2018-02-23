7978a07fce3e52ee275232978b354cb80ebf6349
lihengz <lihengz@yahoo.com>
2018-02-22 22:44:30 -0800

    mysql funcitons

--- MySQL_files/functions/check_g_in_m.sql
+++ MySQL_files/functions/check_g_in_m.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `check g_in_m`(gid integer, mid varchar(10)) RETURNS varchar(1) CHARSET utf8
+CREATE DEFINER=`root`@`localhost` FUNCTION `check_g_in_m`(gid integer, mid varchar(10)) RETURNS varchar(1) CHARSET utf8
 BEGIN
 	IF(select exists(select 1 from genres_in_movies where genreId LIKE gid AND movieId LIKE mid))THEN
 		return '1';
