9457d1317b6f8f2fafaf56d2c72e9766e84ceb2c
lihengz <lihengz@yahoo.com>
2018-03-05 14:46:51 -0800

    Login page is looking good

---
+++ App/app/src/main/java/com/example/a2changz/ldmovie/launch_login.java
@@ -0,0 +1,33 @@
+package com.example.a2changz.ldmovie;
+
+/**
+ * Created by 2Changz on 3/5/2018.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class launch_login extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+
+
+    }
+
+
+
+
+}
