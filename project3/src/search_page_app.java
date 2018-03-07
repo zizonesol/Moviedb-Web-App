cfb207fced3574e93197867449c516fe7d5ef14a
lihengz <lihengz2@uci.edu>
2018-03-06 19:44:36 -0800

    searchpage up

--- project3/src/search_page_app.java
+++ project3/src/search_page_app.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class search_page_app
@@ -25,13 +27,13 @@ public class search_page_app extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		response.setContentType("application/json");
-		ServletOutputStream out = response.getOutputStream();
+		PrintWriter out = response.getWriter();
 		
 		try {
 		
-			String loginUser = "mytestuser";
-			String loginPasswd = "mypassword";
-			String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+			String loginUser = "lihengz2";
+	        String loginPasswd = "as499069589";
+	        String loginUrl = "jdbc:mysql://ec2-52-53-153-231.us-west-1.compute.amazonaws.com:3306/moviedb";
 			        
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
@@ -39,25 +41,83 @@ public class search_page_app extends HttpServlet {
 			Statement statement = dbcon.createStatement();
 			
 			String name = request.getParameter("movie_title");
-			String query = "select * , r.rating from\r\n" + 
-            		"	(select s.title,s.year,s.director,GROUP_CONCAT(DISTINCT ss.name) as Stars_Appear,group_concat(DISTINCT gs.name) as geners_list\r\n" + 
-            		"	from movies s, stars_in_movies sm, stars ss,genres_in_movies gm, genres gs\r\n" + 
-            		"	where gs.id = gm.genreId\r\n" + 
-            		"		AND gm.movieId = s.id\r\n" + 
-            		"		AND ss.id = sm.starId\r\n" + 
-            		"        AND sm.movieId = s.id\r\n AND s.title LIKE '%" + name + "%'\r\n" + 
-            		"	Group by s.id) as masterp , ratings r , movies m\r\n" + 
-            		"    where m.title = masterp.title\r\n" + 
-            		"    AND m.id = r.movieId \r\n" +
-            		"limit 10\r\n"+
-            		"OFFSET 0;";
+			
+			String[] sear = name.split(" ");
+			String srq = "title LIKE \"%" + name + "%\"";
+			if(sear.length > 1)
+			{
+				String zz = "match(title) against(\"";
+				for (int z = 0; z < sear.length ; z++)
+				{
+					zz = zz + "+" +sear[z] + "*,";
+				}
+				zz = zz.substring(0,zz.length()-1);
+				zz = zz + "\" in boolean mode)";
+				srq = zz;
+			}
+			
+			
+			String query = "select title, year, director,GROUP_CONCAT(DISTINCT sname) as Stars_Appear,group_concat(DISTINCT gname) as geners_list\r\n" + 
+					"	from\r\n" + 
+					"(select sel.id, sel.title, sel.year, sel.director, s.name as sname , g.name as gname from \r\n" + 
+					"		((select * from movies where title like \"%good%\") as sel\r\n" + 
+					"		left join stars_in_movies sm on sm.movieId=sel.id\r\n" + 
+					"		left join stars s on sm.starId=s.id\r\n" + 
+					"		left join genres_in_movies gm on gm.movieId=sel.id\r\n" + 
+					"		left join genres g on g.id=gm.genreId)) as re\r\n" + 
+					"group by re.id;";
 			ResultSet rs = statement.executeQuery(query);
+			
+			JSONObject jout = new JSONObject();
+			JSONArray ja = new JSONArray();
 			
 			while(rs.next())
 			{
+				String m_title = rs.getString("title");
+				Integer m_year = rs.getInt("year");
+				String m_director = rs.getString("director");
+				String m_stars = rs.getString("Stars_Appear");
+				String m_genre = rs.getString("geners_list");
 				
+				JSONObject jj = new JSONObject();
+				if(m_stars != null) {
+					String sstar = m_stars.replaceAll(",", ", ");
+					sstar = sstar.substring(0,sstar.length()-2);
+					jj.put("all_star", sstar);
+				}
+				else
+				{
+					jj.put("all_star", "None");
+				}
+				
+				if(m_year == 0)
+				{
+					jj.put("year", "Unknown");
+				}
+				else
+				{
+					jj.put("year", String.valueOf(m_year));
+				}
+				System.out.println("HI");
+				if(m_genre == null)
+				{
+					jj.put("genre", "None");
+				}
+				else
+				{
+					m_genre = m_genre.replaceAll(",", ", ");
+					m_genre = m_genre.substring(0, m_genre.length()-2);
+					jj.put("genre", m_genre);
+				}
+				
+				jj.put("title", m_title);
+				jj.put("director", m_director);
+				
+				ja.put(jj);
 			}
 			
+			jout.put("all_movie",ja);
+			out.print(jout.toString());
 			
 			rs.close();
 			statement.close();
