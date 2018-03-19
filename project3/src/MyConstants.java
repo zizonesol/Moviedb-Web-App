b73c8bf058d7fa3a997f24ed39b9e97cb361d3cc
lihengz <lihengz@yahoo.com>
2018-03-19 09:51:43 -0700

    deletestuff

--- project3/src/MyConstants.java
+++
@@ -1,7 +0,0 @@
-
-public class MyConstants 
-{
-	public static final String SITE_KEY = "6LdilEcUAAAAALullJ_Y3PQs3m_MfvRBXOURG4Ua";
-	
-	public static final String SECRET_KEY = "6LdilEcUAAAAAICrfyZDxK5SSxKYw3AtGc2Y5DSb";
-}
